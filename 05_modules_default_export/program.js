ad95503f98a163c60f0542aa87090de30d27dd91
Janis Stipnieks <j.stipnieks@gmail.com>
2016-05-26 16:23:48 +0200

    05 modules export default

---
+++ 05_modules_default_export/program.js
@@ -0,0 +1,8 @@
+import Module from './module';
+
+let arg1 = process.argv[2];
+let arg2 = process.argv[3];
+
+console.log(Module.PI);
+console.log(Module.sqrt(arg1));
+console.log(Module.square(arg2));
