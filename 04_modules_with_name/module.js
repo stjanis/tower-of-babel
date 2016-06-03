aed14f73ef39c98376f27d9acd24bfc7701b5d75
stjanis <janis.stipnieks@novasol.com>
2016-06-03 12:57:09 +0200

    06 block scope

--- 04_modules_with_name/module.js
+++ 04_modules_with_name/module.js
@@ -2,6 +2,5 @@ export const PI = 3.141592;
 
 let _sqrt = (s, x, last) => x != last ? _sqrt(s, (x + s / x) / 2.0, x) : x;
 
-
 export let sqrt = (s) => _sqrt(s, s/2.0, 0.0);
 export let square = (x) => x * x;
--- 05_modules_default_export/module.js
+++ 05_modules_default_export/module.js
@@ -1,11 +1,11 @@
-const PI = 3.141592;
+//const PI = 3.141592;
 
 let _sqrt = (s, x, last) => x != last ? _sqrt(s, (x + s / x) / 2.0, x) : x;
 let sqrt = (s) => _sqrt(s, s/2.0, 0.0);
 let square = (x) => x * x;
 
 export default {
-  PI: PI,
+  PI: 3.141592,
   sqrt: sqrt,
   square: square
 }
