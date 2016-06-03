83caa1e43b11e7fd70e19b4c3a12a451eacf2720
stjanis <janis.stipnieks@novasol.com>
2016-06-03 16:13:53 +0200

    12 rest and spread: start

---
+++ 12_rest_and_spread.js
@@ -0,0 +1,13 @@
+var rawArgs = process.argv.slice(2);
+var args = [];
+
+rawArgs.forEach(val => {
+  let commaSep = val.split(',');
+  commaSep.forEach(val => {
+    if(val !== '') args.push(+val);
+  });
+});
+
+// write a function called `avg` here that calculates the average.
+
+console.log(avg(...args));
