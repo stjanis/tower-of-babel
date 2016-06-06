d04d7542eb7935d688e657c56bc020add9415791
stjanis <janis.stipnieks@novasol.com>
2016-06-06 10:37:41 +0200

    12 rest and spread: completed

--- 12_rest_and_spread.js
+++ 12_rest_and_spread.js
@@ -1,13 +1,15 @@
 var rawArgs = process.argv.slice(2);
-var args = [];
+var arr = [];
 
 rawArgs.forEach(val => {
   let commaSep = val.split(',');
   commaSep.forEach(val => {
-    if(val !== '') args.push(+val);
+    if(val !== '') arr.push(+val);
   });
 });
 
 // write a function called `avg` here that calculates the average.
-
-console.log(avg(...args));
+let avg = function(...args) {
+  return args.reduce((a,b) => a + b) / args.length;
+}
+console.log(avg(...arr));
